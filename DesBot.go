6bcadd7a33b194614ce40de711b696af2f948a7e
DesRoin <florian.kanngiesser@googlemail.com>
2024-02-20 14:46:44 +0100

    Added code from Tutorial and tested first connection xD

--- DesBot.go
+++ DesBot.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"context"
+	"github.com/mattn/go-mastodon"
 )
 	
 
@@ -14,4 +16,21 @@ func main() {
   }
 
   fmt.Printf("%v", envs)
+  
+   c := mastodon.NewClient(&mastodon.Config{
+    Server:       envs["MASTODON_SERVER"],
+    ClientID:     envs["APP_CLIENT_ID"],
+    ClientSecret: envs["APP_CLIENT_SECRET"],
+  })
+  err := c.Authenticate(context.Background(), envs["APP_USER"], envs["APP_PASSWORD"])
+  if err != nil {
+    log.Fatal(err)
+  }
+  timeline, err := c.GetTimelineHome(context.Background(), nil)
+  if err != nil {
+    log.Fatal(err)
+  }
+  for i := len(timeline) - 1; i >= 0; i-- {
+    fmt.Println(timeline[i])
+  }
 }
